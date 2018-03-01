e0d0c407ea00bbc3cf4a9cc675c49f534dc48b8a
Thomas Alves de Souza Abrantes <abrantes.thomas@gmail.com>
2018-03-01 14:56:39 -0300

    implementaçao do Rotativa para gerar relatorios

--- SINTALOCAS.Web.MVC/Controllers/RelatorioController.cs
+++ SINTALOCAS.Web.MVC/Controllers/RelatorioController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using SINTALOCAS.Dominio.Servico;
 using SINTALOCAS.Web.MVC.Servico;
+using Rotativa;
+using SINTALOCAS.Dominio.Util;
 
 namespace SINTALOCAS.Web.MVC.Controllers
 {
@@ -47,14 +49,14 @@ namespace SINTALOCAS.Web.MVC.Controllers
         public ActionResult DetalheAfiliado(string id)
         {
             var listaAfiliado = AfiliacaoServico.Listar(id.ToString());
-            
+            TempData["cpfAfiliado"] = id;
             ViewBag.Lista = listaAfiliado;
 
             if (listaAfiliado.Count > 0)
             {
                 var idAfiliado = listaAfiliado[0].ID;
+                
                 ViewBag.Dependentes = DependenteServico.ListaDependentes(idAfiliado);
-
                 ViewBag.Enderecos = listaAfiliado[0].Endereco;
             }
 
@@ -67,7 +69,7 @@ namespace SINTALOCAS.Web.MVC.Controllers
             ViewBag.ListaRelatorio = RelatorioServico.ListarRelatorios();
         }
         
-        private void GeraViewBagRelatorio(int opcaoRelatorio)
+        private void GeraViewBagRelatorio(int opcaoRelatorio, string id = "")
         {
             LogAtivo();
 
@@ -77,6 +79,60 @@ namespace SINTALOCAS.Web.MVC.Controllers
             if (opcaoRelatorio == 2)
                 ViewBag.Lista = LogServico.ListaAgrupado();
 
+            if (opcaoRelatorio == 3)
+            {
+                var listaAfiliado = AfiliacaoServico.Listar(id.ToString());
+
+                ViewBag.Lista = listaAfiliado;
+
+                if (listaAfiliado.Count > 0)
+                {
+                    var idAfiliado = listaAfiliado[0].ID;
+                    ViewBag.Dependentes = DependenteServico.ListaDependentes(idAfiliado);
+
+                    ViewBag.Enderecos = listaAfiliado[0].Endereco;
+                }
+            }
+
+        }
+
+        public ActionResult PDFPadrao(int id)
+        {
+            var viewRelatorio = "";
+
+            if (id == 2)
+            {
+                GeraViewBagRelatorio(id);
+                viewRelatorio = "RelLogPDF";
+            }
+            if (id == 1)
+            {
+                GeraViewBagRelatorio(id);
+                viewRelatorio = "RelAfiliadosPDF";
+            }
+            if (id == 3)
+            {
+                if (TempData["cpfAfiliado"] != null)
+                {
+                    var cpfAfiliado = TempData["cpfAfiliado"].ToString();
+                    GeraViewBagRelatorio(id, cpfAfiliado);
+                    viewRelatorio = "DetalheAfiliadoPDF";
+                }
+            }
+
+            if (viewRelatorio.Trim() != "")
+            {
+                var pdf = new ViewAsPdf
+                {
+                    ViewName = viewRelatorio
+                };
+                return pdf;
+            }
+            else
+            {
+                ViewBag.MensagemRetorn0 = MensagemUtil.ErroGeneralizado();
+                return View();
+            }
         }
 
         private void LogAtivo()
